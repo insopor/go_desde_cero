ec5f4588c106996ed72f734086968f194fee6f80
insopor <garturorf@gmail.com>
2026-03-11 23:52:27 -0600

    se agrega el webserver para levantar un http en un puerto

--- main.go
+++ main.go
@@ -15,10 +15,12 @@ import (
 
 	"github.com/insopor/go_desde_cero/deferPanic"
 	ei "github.com/insopor/go_desde_cero/ejerciciosInterface"
-	gorutines "github.com/insopor/go_desde_cero/goRutines"
+
+	//gorutines "github.com/insopor/go_desde_cero/goRutines"
 	"github.com/insopor/go_desde_cero/modelos"
 	"github.com/insopor/go_desde_cero/usuarios"
 	"github.com/insopor/go_desde_cero/variables"
+	"github.com/insopor/go_desde_cero/webserver"
 )
 
 var r1 bool
@@ -118,19 +120,23 @@ func main() {
 		fmt.Scanln(&x)
 	*/
 
-	//de esta manera podemos hacer que la ejecución termine hasta que termine de ejecutarse la rutina
-	canal1 := make(chan bool) //creamos el canal para inyectar
-	go gorutines.MiNombreLentoChanel("arturo", canal1)
-	fmt.Println("estoy aqui")
-	<-canal1 // asi se declara el await o la espera a que se termine la rutina
+	/*
+		//de esta manera podemos hacer que la ejecución termine hasta que termine de ejecutarse la rutina
+		canal1 := make(chan bool) //creamos el canal para inyectar
+		go gorutines.MiNombreLentoChanel("arturo", canal1)
+		fmt.Println("estoy aqui")
+		<-canal1 // asi se declara el await o la espera a que se termine la rutina
 
-	//también de esta manera podemos declarar el await dentro de una funcion anonima
-	// y dentro de la funcion también podemos declarar código extra
-	canal2 := make(chan bool)
-	go gorutines.MiNombreLentoChanel("beto", canal2)
-	defer func() {
-		<-canal2
-	}()
-	fmt.Println("estoy aqui 2")
+		//también de esta manera podemos declarar el await dentro de una funcion anonima
+		// y dentro de la funcion también podemos declarar código extra
+		canal2 := make(chan bool)
+		go gorutines.MiNombreLentoChanel("beto", canal2)
+		defer func() {
+			<-canal2
+		}()
+		fmt.Println("estoy aqui 2")
+	*/
+
+	webserver.MiWebSerbver()
 
 }
